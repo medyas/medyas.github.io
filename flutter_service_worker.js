339ba3d35d50dcfed57545894d3b8fdc304b01c6
medyas <medyassinesabri@gmail.com>
2020-05-22 23:26:32 +0100

    updated build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,15 +1,14 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "6575773d4aef24a098fe6e6901b07c35",
+  "assets/AssetManifest.json": "48abae97aff21f3633e06e577a952345",
 "assets/assets/github.svg": "ddc250127189a3b4ee4802e6c2493757",
 "assets/assets/linkedin.svg": "4b475fb3f54b3e35daa04295ec48df4c",
 "assets/assets/profile_image.jpg": "ea3819a5512cbda2481a719f32f5a5f1",
 "assets/assets/twitter.svg": "c9f1200836515fd4bacd29160be78732",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "b9011baf50a2a7b7519041c3ae57935e",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/LICENSE": "2a4ff25ab709f05e8d93d21075cb70cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
